215b66a1dc844030adb5c9553fb20addd74d805f
Sabrina Ferreira <sabrinaferreirasilva745@gmail.com>
2026-03-03 11:43:14 -0300

    .

--- Gerador-Css/config.js
+++ Gerador-Css/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-  VITE_GROQ_API_KEY: "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",
+  VITE_GROQ_API_KEY: import.meta.env.VITE_GROQ_API_KEY,
 };
-
+export default config;
 
